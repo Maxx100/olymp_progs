9c5f310e0f80686c7d28694a8d1340d3db16fbcf
Max <rezonyou@gmail.com>
2024-08-05 12:43:58 +0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,44 @@
+// only if necessary
+/*#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("O3")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma warning(disable:4996)*/
+
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <numeric>
+// for int**_max/min, only for yandex contests
+// #include <cstdint>
+
+#define ll long long
+#define ull unsigned long long
+#define INF INT64_MAX
+#define EPS 0.00000001
+#define m1 1000000007
+#define m2 1000000009
+#define p 27
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define files freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout)
+#define prec cout.precision(15)
+
+using namespace std;
+
+void solve() {
+    
+}
+
+int main() {
+    fast_io;
+    ll t = 1; // cin >> t; // if each test contains several cases
+    while (t--) solve();
+    return 0;
+}
